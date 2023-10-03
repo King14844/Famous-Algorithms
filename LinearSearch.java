e2565d0d201473351a176547b65a10061749af3e
King14844 <91273036+King14844@users.noreply.github.com>
2023-10-03 22:57:38 +0530

    Create LinearSearch.java
    Time Complexity - O(N)
    Space Complexity - O(1)

---
+++ LinearSearch.java
@@ -0,0 +1,22 @@
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10};
+        int target = 6;
+        int result = linearSearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index " + result);
+        } else {
+            System.out.println("Element not found in the array");
+        }
+    }
+}
